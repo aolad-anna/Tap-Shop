57dbb56e290f135e38087a2fc2a9d316ee8d8a3d
Aolad Hossain Anna <aolad.hossain@nexdecade.com>
2023-04-04 23:28:10 +0600

    checkout process multiple product

--- app/src/main/java/com/example/tap_shop/ui/home/ProductDetailsBottomSheetFragment.kt
+++ app/src/main/java/com/example/tap_shop/ui/home/ProductDetailsBottomSheetFragment.kt
@@ -1,24 +1,26 @@
 package com.example.tap_shop.ui.home
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import coil.load
 import com.example.tap_shop.R
-import com.example.tap_shop.databinding.BottomSheetDialogBinding
+import com.example.tap_shop.databinding.ProductDetailsBottomSheetDialogBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ProductDetailsBottomSheetFragment : BottomSheetDialogFragment() {
+    private var count: Int = 0
+    private var updatePrice: Float = 0F
     private val viewModel by activityViewModels<HomeViewModel>()
 //    private val progressDialog by unsafeLazy { DialogLoader (requireContext()) }
+    @SuppressLint("SetTextI18n")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
-        val dialogBinding = BottomSheetDialogBinding.inflate(layoutInflater)
+        val dialogBinding = ProductDetailsBottomSheetDialogBinding.inflate(layoutInflater)
 
         dialog.setContentView(dialogBinding.root)
         dialog.setCancelable(false)
@@ -35,19 +37,42 @@ class ProductDetailsBottomSheetFragment : BottomSheetDialogFragment() {
             dialogBinding.imageArtiest.load(it?.images?.get(0))
             dialogBinding.titleName.text = it?.title
             dialogBinding.priceName.text = "Price: $${it?.price.toString()}"
-            dialogBinding.descriptionLabel.text = "${it?.description.toString()}"
+            dialogBinding.descriptionLabel.text = it?.description.toString()
 
-            dialogBinding.playBtn.setOnClickListener {
-//                val args = bundleOf(
-//                    "album" to albumTG,
-//                    "title" to titleTG,
-//                    "artistDisplayName" to artistDisplayNameTG,
-//                    "urlPlayable" to urlPlayable,
-//                )
-//                findNavController().navigate(
-//                    R.id.playerViewFragment,
-//                    args
-//                )
+            dialogBinding.add.setOnClickListener {
+                dialogBinding.subs.isEnabled = true
+                dialogBinding.checkOutBtn.isEnabled = true
+                count += 1
+                dialogBinding.totalPriceBox.text = count.toString()
+                val first = viewModel.selectedProducts?.price?.toFloat()
+                updatePrice = first!! * count.toFloat()
+                dialogBinding.totalPrice.text = "TotalPrice: $$updatePrice"
+            }
+
+            if (count == 0){
+                dialogBinding.subs.isEnabled = false
+                dialogBinding.checkOutBtn.isEnabled = false
+            }
+
+            dialogBinding.subs.setOnClickListener {
+                if (count == 1){
+                    dialogBinding.subs.isEnabled = false
+                    dialogBinding.checkOutBtn.isEnabled = false
+                }
+                count -= 1
+                dialogBinding.totalPriceBox.text = count.toString()
+                updatePrice = updatePrice!! - viewModel.selectedProducts?.price?.toFloat()!!
+                dialogBinding.totalPrice.text = "TotalPrice: $$updatePrice"
+            }
+
+            dialogBinding.checkOutBtn.setOnClickListener {
+                dialogBinding.detailsView.visibility = View.GONE
+                dialogBinding.successView.visibility = View.VISIBLE
+                dialogBinding.totalPrice2.text = "Total Payable Amount: $$updatePrice"
+            }
+
+            dialogBinding.continueBtn.setOnClickListener {
+                dialog.dismiss()
             }
 
         }
